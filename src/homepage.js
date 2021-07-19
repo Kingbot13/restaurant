92fedb36c112005163986b6a5514c94cc973da95
Dylan King <dwkblue51@yahoo.com>
2021-07-19 14:10:05 -0500

    add homepage module and link to index.js

--- src/homepage.js
+++ src/homepage.js
@@ -1,1 +1,28 @@
 // DOM for homepage of restaurant project
+
+const homepage = (() => {
+    // main div container
+    const container = document.createElement('div');
+    // content to append to container
+
+    // title
+    const titleContainer =  document.createElement('div');
+    const title = document.createElement('h1');
+    title.textContent = `Title`;
+    titleContainer.appendChild(title);
+
+    // homepage copywriting
+    const text = document.createElement('p');
+    text.textContent = `Welcome, Have a look around at our tasty selection. We know your tastebuds 
+    will be begging you to try our food. So what are you waiting for? 
+    If you're tired of being hungry, stop by or place an order over 
+    the phone!`;
+
+    container.append(titleContainer, text);
+
+    return {container};
+
+})();
+
+
+export default homepage;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 // hub for all modules to be used
 import nav from "./nav.js";
+import home from "./homepage";
 
 const page = (() => {
 
     const displayAppend = () => {
         const display = document.querySelector("#content");
         display.appendChild(nav.createNav());
+
+        // append homepage on initial load
+
+        display.appendChild(home.container);
     };
     
     return {displayAppend}
